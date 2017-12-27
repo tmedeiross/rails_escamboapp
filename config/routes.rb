d809264e673652ecc6cb9c337d2157ab1b2dd7f4
Tatiana Medeiros <tatiana@chdigital.com.br>
2017-12-27 16:18:34 -0200

    proj

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  namespace :backoffice do
+    get 'dashboard/index'
+  end
+
+  devise_for :admins
+  devise_for :members
+  get 'home/index'
+  root 'home#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
